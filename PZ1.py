7a15db0e470057e2a2f3875bbf64237e8e9d99c3
Daniel Jug (djug) <djug@tvz.hr>
2023-04-04 20:17:14 +0200

    popravljeni kod

--- PZ1.py
+++ PZ1.py
@@ -1,49 +1,56 @@
 import datetime
 
-# Inicijaliziramo prazne rječnike za korisnika, artikl i prodaju
-korisnik = {}
-artikl = {}
-prodaja = {}
+# unos korisnikovih podataka
+ime = input("Unesite ime korisnika: ").capitalize()
+prezime = input("Unesite prezime korisnika: ").capitalize()
+telefon = int(input("Unesite telefon korisnika: "))
+email = input("Unesite email korisnika: ").strip()
 
-# Korisnik unosi podatke o sebi
-ime = input("Unesite ime: ").title()
-prezime = input("Unesite prezime: ").title()
-telefon = int(input("Unesite telefon: "))
-email = input("Unesite email: ").strip()
+# spremanje korisnikovih podataka u rječnik
+korisnik = {
+    "ime": ime,
+    "prezime": prezime,
+    "telefon": telefon,
+    "email": email
+}
 
-# Spremamo podatke o korisniku u rječnik
-korisnik["ime"] = ime
-korisnik["prezime"] = prezime
-korisnik["telefon"] = telefon
-korisnik["email"] = email
-
-# Korisnik unosi podatke o artiklu
+# unos podataka o artiklu
 naslov = input("Unesite naslov artikla: ")
 opis = input("Unesite opis artikla: ")
 cijena = float(input("Unesite cijenu artikla: "))
 
-# Spremamo podatke o artiklu u rječnik
-artikl["naslov"] = naslov
-artikl["opis"] = opis
-artikl["cijena"] = cijena
+# spremanje podataka o artiklu u rječnik
+artikl = {
+    "naslov": naslov,
+    "opis": opis,
+    "cijena": cijena
+}
 
-# Inicijaliziramo datum prodaje kao današnji datum
-datum = datetime.date.today()
+# unos podataka o prodaji
+dan = int(input("Unesite dan isteka prodaje: "))
+mjesec = int(input("Unesite mjesec isteka prodaje: "))
+godina = int(input("Unesite godinu isteka prodaje: "))
 
-# Spremamo podatke o prodaji u rječnik
-prodaja["datum"] = datum
-prodaja["artikl"] = artikl
-prodaja["korisnik"] = korisnik
+# spremanje podataka o prodaji u rječnik
+prodaja = {
+    "datum": datetime.date(godina, mjesec, dan),
+    "artikl": artikl,
+    "korisnik": korisnik
+}
 
-# Ispisujemo sve informacije o prodaji
-print("Prodaja:")
-print("Datum: ", prodaja["datum"])
-print("Korisnik: ", prodaja["korisnik"]["ime"], prodaja["korisnik"]["prezime"])
-print("Telefon: ", prodaja["korisnik"]["telefon"])
-print("Email: ", prodaja["korisnik"]["email"])
-print("Artikl: ", prodaja["artikl"]["naslov"])
-print("Opis: ", prodaja["artikl"]["opis"])
-print("Cijena: ", prodaja["artikl"]["cijena"])
+# ispis podataka o prodaji
+print("Informacije o artiklu:")
+print("Naslov:", prodaja["artikl"]["naslov"])
+print("Opis:", prodaja["artikl"]["opis"])
+print("Cijena:", prodaja["artikl"]["cijena"])
+print("Datum isteka prodaje:")
+print("Dan:", prodaja["datum"].day)
+print("Mjesec:", prodaja["datum"].month)
+print("Godina:", prodaja["datum"].year)
+print("Informacije o korisniku:")
+print(prodaja["korisnik"]["ime"], prodaja["korisnik"]["prezime"])
+print("Telefon:", prodaja["korisnik"]["telefon"])
+print("Email:", prodaja["korisnik"]["email"])
 
 
 
